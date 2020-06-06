7f12212fba4f35f0a74d8c28ff1866538c139814
Nimitha J <nimitha1jagadeesha@gmail.com>
2020-06-06 18:37:13 +0530

    Updates

---
+++ precache-manifest.387bb3e134a17ccbbd871ca0f4ea6f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca519ed95cf659a122e6553e0128bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48bb077b14d3d077a10",
+    "url": "/static/css/main.6971dfda.chunk.css"
+  },
+  {
+    "revision": "6506b5190b1aec541b73",
+    "url": "/static/js/2.24388a56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24388a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48bb077b14d3d077a10",
+    "url": "/static/js/main.9b50509d.chunk.js"
+  },
+  {
+    "revision": "f74a583a02143f69ad0c",
+    "url": "/static/js/runtime-main.7a0bc159.js"
+  }
+]);
